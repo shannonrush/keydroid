4b7205f311422fd9f3226266e70ab78ae1d1e228
Shannon Rush <shannonmrush@gmail.com>
2014-03-23 16:23:34 -0600

    adding user and contacts tables

--- app/src/main/java/com/rushdevo/keydroid/app/db/KeydroidDatabaseHelper.java
+++ app/src/main/java/com/rushdevo/keydroid/app/db/KeydroidDatabaseHelper.java
@@ -2,10 +2,19 @@ package com.rushdevo.keydroid.app.db;
 
 
 import android.database.sqlite.SQLiteOpenHelper;
+import static android.provider.BaseColumns._ID;
 
 public class KeydroidDatabaseHelper extends SQLiteOpenHelper {
-    public static final String TABLE_USERS = "users";
-    public static final String TABLE_CONTACTS = "contacts";
+    // Tables
+    private static final String TABLE_USERS = "users";
+    private static final String TABLE_CONTACTS = "contacts";
+
+    // User Fields
+    private static final String USERS_KEYBASE_ID = "keybase_id";
+
+    // Contact Fields
+    private static final String CONTACTS_KEYBASE_ID = "keybase_id"; // keybase_id is the Keybase.io username
+    private static final String CONTACTS_KEYDROID_ID = "keydroid_id"; // keydroid_id is the keydroid app identifier
 
     private static final String DATABASE_NAME = "keydroid.db";
     private static final int DATABASE_VERSION = 1;
@@ -14,12 +23,25 @@ public class KeydroidDatabaseHelper extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
     public void onCreate(android.database.sqlite.SQLiteDatabase db) {
-        db.execSQL(SQL_CREATE_ENTRIES);
+        // Users Table
+        StringBuilder user_sql = new StringBuilder();
+        user_sql.append("CREATE TABLE " + TABLE_USERS + " (");
+        user_sql.append(_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, ");
+        user_sql.append(USERS_KEYBASE_ID + " TEXT");
+        user_sql.append(");");
+        db.execSQL(user_sql.toString());
+
+        // Contacts Table
+        StringBuilder contact_sql = new StringBuilder();
+        contact_sql.append("CREATE TABLE " + TABLE_CONTACTS + " (");
+        contact_sql.append(_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, ");
+        contact_sql.append(CONTACTS_KEYBASE_ID + " TEXT, ");
+        contact_sql.append(CONTACTS_KEYDROID_ID + " TEXT");
+        contact_sql.append(");");
+        db.execSQL(contact_sql.toString());
     }
     public void onUpgrade(android.database.sqlite.SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
-
-
 
 }
